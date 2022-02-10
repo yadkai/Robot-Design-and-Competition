ddbdd410511e2800e40caba37d762cc387b060b8
Sajeepan <Sajeepan1299@gmail.com>
2022-02-10 21:54:32 +0530

    file organization

--- LINE_&_MAZE/Robot_Competition/controllers/LINE_FOLLOW_PID/build/release/LINE_FOLLOW_PID.d
+++
@@ -1,6 +0,0 @@
-build/release/LINE_FOLLOW_PID.o: LINE_FOLLOW_PID.cpp \
- C:\Program\ Files\Webots/include/controller/cpp/webots/Robot.hpp \
- C:\Program\ Files\Webots/include/controller/cpp/webots/Motor.hpp \
- C:\Program\ Files\Webots/include/controller/cpp/webots/Device.hpp \
- C:/Program\ Files/Webots/include/controller/c/webots/types.h \
- C:\Program\ Files\Webots/include/controller/cpp/webots/DistanceSensor.hpp
---
+++ sub_tasks/robot_movement/LINE_&_MAZE/Robot_Competition/controllers/LINE_FOLLOW_PID/build/release/LINE_FOLLOW_PID.d
@@ -0,0 +1,6 @@
+build/release/LINE_FOLLOW_PID.o: LINE_FOLLOW_PID.cpp \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/Robot.hpp \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/Motor.hpp \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/Device.hpp \
+ C:/Program\ Files/Webots/include/controller/c/webots/types.h \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/DistanceSensor.hpp
