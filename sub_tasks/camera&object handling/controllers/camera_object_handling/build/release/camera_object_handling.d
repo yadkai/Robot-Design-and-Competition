da575788d4b8aebf51af2b68b395e6d0136f39a0
Sajeepan <Sajeepan1299@gmail.com>
2022-02-11 14:42:25 +0530

    a

--- sub_tasks/camera&object handling/controllers/camera_object_handling/build/release/camera_object_handling.d
+++
@@ -1,7 +0,0 @@
-build/release/camera_object_handling.o: camera_object_handling.cpp \
- D:\Program\ Files\Webots/include/controller/cpp/webots/Robot.hpp \
- D:\Program\ Files\Webots/include/controller/cpp/webots/Motor.hpp \
- D:\Program\ Files\Webots/include/controller/cpp/webots/Device.hpp \
- D:/Program\ Files/Webots/include/controller/c/webots/types.h \
- D:\Program\ Files\Webots/include/controller/cpp/webots/DistanceSensor.hpp \
- D:\Program\ Files\Webots/include/controller/cpp/webots/PositionSensor.hpp
