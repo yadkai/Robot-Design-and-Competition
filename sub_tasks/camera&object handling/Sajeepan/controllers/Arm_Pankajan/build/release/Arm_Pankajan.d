3d945b83edd2626115a0e556f8d817d79306b5e2
Sajeepan <Sajeepan1299@gmail.com>
2022-02-13 09:56:04 +0530

    robot some properties changed

---
+++ sub_tasks/camera&object handling/Sajeepan/controllers/Arm_Pankajan/build/release/Arm_Pankajan.d
@@ -0,0 +1,7 @@
+build/release/Arm_Pankajan.o: Arm_Pankajan.cpp \
+ D:\Program\ Files\Webots/include/controller/cpp/webots/Robot.hpp \
+ D:\Program\ Files\Webots/include/controller/cpp/webots/Motor.hpp \
+ D:\Program\ Files\Webots/include/controller/cpp/webots/Device.hpp \
+ D:/Program\ Files/Webots/include/controller/c/webots/types.h \
+ D:\Program\ Files\Webots/include/controller/cpp/webots/DistanceSensor.hpp \
+ D:\Program\ Files\Webots/include/controller/cpp/webots/PositionSensor.hpp
